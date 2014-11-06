66617989ef5066874ac1f68dd35c5b101ac150ed
warnerp18 <warnerp18@gmail.com>
2014-11-05 21:20:20 -0600

    working on it

--- js/WeatherClient.js
+++ js/WeatherClient.js
@@ -1,6 +1,28 @@
 (function(window, undefined) {
-    "use strict";
-    var WeatherView = Backbone.View.extend({})
+        "use strict";
+        var WeatherView = Backbone.View.extend({
+                className: "weather",
+                initialize: function(opts) {
+                    // 1. Sometimes it will be instantiated without options, so to guard against errors:
+                    this.options = _.extend({}, {
+                            $container: $('body')
+                        },
+                        opts
+                    );
+                    // 2. Part of putting a view into its initial state is to put its element
+                    //    into the DOM. Its container should be configurable using an option
+                    //    so that a) it can be used anywhere in the app and b) it can be
+                    //    easily unit tested.
+                    this.options.$container.append(this.el);
+
+                    // 3. Render the content of the view
+                    this.render();
+                },
+                template: "<h1>{name}</h1><hr><ul><li>{location.lat}</li><li>{location.lng}</li></ul>",
+                render: function() {
+                    this.el.innerHTML = _.template(this.template, this.options);
+                }
+        })
 
     function WeatherClient(options) {
         this.options = _.extend({}, options, {
@@ -43,8 +65,8 @@
         var self = this;
 
         $.when(
-        this.queryAPI("TX", "Houston")
-        ).then(function(TX, Houston) {
+            this.queryAPI("TX", "Houston")
+        ).then(function(/*TX, Houston*/) {
             self.makeWeatherUndergroundRequest(data);
         })
 
